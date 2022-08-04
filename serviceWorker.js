9ec00550249c0478dabcbf9480521d5933e1dbdb
Trino62 <35821194+Trino62@users.noreply.github.com>
2022-08-04 13:06:25 -0500

    change project

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,29 +1,32 @@
-const staticDevCoffee = "TimerRoundV1"
+const staticDevCoffee = "dev-coffee-site-v1";
 const assets = [
   "/",
   "/index.html",
-  "/style.css",
-  "/src/funciones.js",
-  "/src/index.js",
-  "/src/reloj.js",
-  "/src/round.js",
-  "/src/images/icon_boxing_timer.png",
-  "/src/images/killerbeeslogo.png",
-
-]
+  "/css/style.css",
+  "/js/app.js",
+  "/images/coffee1.jpg",
+  "/images/coffee2.jpg",
+  "/images/coffee3.jpg",
+  "/images/coffee4.jpg",
+  "/images/coffee5.jpg",
+  "/images/coffee6.jpg",
+  "/images/coffee7.jpg",
+  "/images/coffee8.jpg",
+  "/images/coffee9.jpg"
+];
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
+      cache.addAll(assets);
     })
-  )
-})
+  );
+});
 
 self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
-      })
-    )
-  })
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
